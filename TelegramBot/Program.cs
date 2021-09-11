45e6b7de8e63286a2f539ceb839abafd7dbc2259
Вова <74742074+WoWwD@users.noreply.github.com>
2021-09-11 12:20:44 +0300

    Update Program.cs

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace TelegramBot
+﻿using System;
+
+namespace TelegramBot
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Бот в Telegram - @mmfrtbot");
             Bot bot = new Bot("1674698636:AAHjunKIirrDECO0xHe3f72UvbogU25T6AM"); 
             bot.GetUpdates();
         }
