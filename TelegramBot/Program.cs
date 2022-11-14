883e2e307c3094d81f5d35081bd2f024bf4246a5
Vova <kandrin2002@mail.ru>
2022-11-14 16:33:50 +0300

    рефакторинг 2

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using TelegramBot.NewsOfVuz;
+using TelegramBot.Parsers;
+using TelegramBot.Services;
+using TelegramBot.UI;
 
 namespace TelegramBot
 {
@@ -6,8 +10,16 @@ namespace TelegramBot
     {
         static void Main(string[] args)
         {
+            Buttons buttons = new Buttons();
+            TimeTableParser timeTableParser = new TimeTableParser();
+            ExamTimeTableParser examTimeTableParser = new ExamTimeTableParser();
+            NewsParser newsParser = new NewsParser();
+            Texts texts = new Texts();
+            ScreenshotService screenshotService = new ScreenshotService();
+            string token = "1674698636:AAHjunKIirrDECO0xHe3f72UvbogU25T6AM";
+
             Console.WriteLine("Бот в Telegram - @mmfrtbot");
-            Bot bot = new Bot("1674698636:AAHjunKIirrDECO0xHe3f72UvbogU25T6AM"); 
+            Bot bot = new Bot(token, buttons, timeTableParser, examTimeTableParser, newsParser, texts, screenshotService); 
             bot.GetUpdates();
         }
     }
