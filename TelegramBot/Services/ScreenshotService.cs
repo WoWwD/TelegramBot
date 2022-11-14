298ebec0b1b92d0e7cda257bf7cd4eb3150cf6a8
Vova <kandrin2002@mail.ru>
2022-11-14 16:32:19 +0300

    сделал создание скриншотов отдельно

---
+++ TelegramBot/Services/ScreenshotService.cs
@@ -0,0 +1,34 @@
+﻿using Freezer.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using TelegramBot.Parsers;
+using static System.Net.WebRequestMethods;
+
+namespace TelegramBot.Services
+{
+    public class ScreenshotService
+    {
+        public FileStream GetScreenshot(int screenWidth, int screenHeight, string screenshotName, string linkGroup)
+        {
+            Guid guid = Guid.NewGuid();
+            var screenshotJob = ScreenshotJobBuilder
+                .Create(linkGroup)
+                .SetBrowserSize(screenWidth, screenHeight)
+                .SetCaptureZone(CaptureZone.FullPage)
+                .SetTrigger(new WindowLoadTrigger());
+            System.IO.File.WriteAllBytes($"{guid}{screenshotName}.png", screenshotJob.Freeze());
+            string[] findFile = Directory.GetFiles(
+                AppDomain.CurrentDomain.BaseDirectory,
+                $"{guid}{screenshotName}.png", 
+                SearchOption.AllDirectories
+            );
+            return System.IO.File.OpenRead(findFile[0]);
+        }
+    }
+}
