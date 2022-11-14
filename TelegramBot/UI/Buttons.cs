c8073e1233109c4423da9518524b517047acc3ac
Vova <kandrin2002@mail.ru>
2022-11-14 16:32:49 +0300

    вынес элементы для UI

--- TelegramBot/UI/Buttons.cs
+++ TelegramBot/UI/Buttons.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Telegram.Bot.Types.ReplyMarkups;
+using TelegramBot.All_Paths;
 
 namespace TelegramBot.UI
 {
@@ -12,6 +13,18 @@ namespace TelegramBot.UI
                 Keyboard = new List<List<KeyboardButton>> { new List<KeyboardButton> { new KeyboardButton("Команды") } },
                 ResizeKeyboard = true
             };
+        }
+        public InlineKeyboardMarkup GetSocialNetworksButtons()
+        {
+            return new InlineKeyboardMarkup(
+                new[]
+                {
+                    new[] {InlineKeyboardButton.WithUrl($"{char.ConvertFromUtf32(0x1F535)}  VK", Constants.vkontakteUrl)},
+                    new[] {InlineKeyboardButton.WithUrl($"{char.ConvertFromUtf32(0x26AB)}  Instagram", Constants.instagramUrl)},
+                    new[] {InlineKeyboardButton.WithUrl($"{char.ConvertFromUtf32(0x26AA)}  Facebook", Constants.facebookUrl)},
+                    new[] { InlineKeyboardButton.WithUrl($"{char.ConvertFromUtf32(0x1F534)}  YouTube", Constants.youtubeUrl)}
+                }
+            );
         }
     }
 }
---
+++ TelegramBot/UI/Texts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace TelegramBot.UI
+{
+    public class Texts
+    {
+        public string GetMenu() =>
+        "Список доступных команд:\n\n" +
+        $"{char.ConvertFromUtf32(0x1F4DA)}  /r[группа] - Расписание группы\n\n" +
+        $"{char.ConvertFromUtf32(0x1F4F0)}  /newsOfWeek - Получить новости за текущую неделю\n\n" +
+        $"{char.ConvertFromUtf32(0x1F3EB)}  /links - Ссылки на социальные сети ПензГТУ\n\n";
+
+        public string GetNews(List<string> news) =>
+            $"{char.ConvertFromUtf32(0x2757)}<b>Новости недели</b>{char.ConvertFromUtf32(0x2757)}" +
+            $"\n\n{string.Join("\n\n", news)}";
+
+        public const string firstLaunch =
+            "Привет!\nЧтобы начать пользоваться ботом, необходимо нажать кнопку \"Команды\", " +
+            "которая появилась под полем для ввода сообщений.";
+
+        public string Loading() => $"{char.ConvertFromUtf32(0x23F3)} Пожалуйста, подождите {char.ConvertFromUtf32(0x23F3)}";
+    }
+}
